de4d63876ae58e46f1c2eb766d25a1b974eee42b
Roy Jiny <royjiny@gmail.com>
2020-08-23 12:08:14 +0300

    fixes

--- utils.h
+++ utils.h
@@ -15,4 +15,4 @@ Error read_input(FILE *input, spmat *A, int *degree, int nof_vertex);
 /*adjacency matrix -----> modularity matrix of group g*/
 Error compute_modularity_matrix(spmat *A, double *g, int *degree, double M, spmat *B_g);
 
-double compute_modularity_value(spmat *B_g, int *g);
+double compute_modularity_value(spmat *B_g, double *g);
