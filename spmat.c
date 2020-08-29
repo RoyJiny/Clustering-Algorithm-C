09c29ef993633f02e45ee9408aeca6fb9f6354ed
Roy Jiny <royjiny@gmail.com>
2020-08-29 16:44:51 +0300

    fixes

--- spmat.c
+++ spmat.c
@@ -298,14 +298,17 @@ void mult_list(const spmat *A, const double *v, double *result)
 double add_to_row_list(const spmat *A, int row_index, double *row, group *g)
 {
 	double sum = 0;
-	printf("trying to set members\n");
-	char *g_members = g->members;
-	printf("set members\n");
-	node **rows = ((list *)(A->handle))->rows;
-	printf("defined rows\n");
-	node *curr_row = *(rows + row_index);
-	printf("defined curr row\n");
 	int curr_index = 0;
+	char *g_members;
+	node **rows;
+	node *curr_row;
+	printf("trying to set members\n");
+	g_members = g->members;
+	printf("set members\n");
+	rows = ((list *)(A->handle))->rows;
+	printf("defined rows\n");
+	curr_row = *(rows + row_index);
+	printf("defined curr row\n");
 	while (curr_row != NULL)
 	{
 		if (curr_row->index == curr_index && *g_members)
