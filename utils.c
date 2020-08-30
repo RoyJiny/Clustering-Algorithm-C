b67c182f3b83844c20359ed2febb159bcd563e1a
Roy Jiny <royjiny@gmail.com>
2020-08-30 14:40:30 +0300

    fixes

--- utils.c
+++ utils.c
@@ -7,12 +7,13 @@
 double dot_product(double *row1, double *row2, int size)
 {
 	double sum = 0;
+	double *row1_runner = row1, *row2_runner = row2;
 	double *end = row1 + size;
 	for (; row1 < end;)
 	{
-		sum += (*row1) * (*row2);
-		row1++;
-		row2++;
+		sum += (*row1_runner) * (*row2_runner);
+		row1_runner++;
+		row2_runner++;
 	}
 
 	return sum;
